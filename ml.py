2d09396ab5ca849191ee1e8af68a4884bf9b78ce
Eddie Federmeyer <eddiefedermeyer@yahoo.com>
2019-09-12 11:46:46 -0500

    Added projects

---
+++ ml.py
@@ -0,0 +1,14 @@
+"""
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# Tensorflow and Keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("TensorFlow Version: " + tf.__version__)
+
