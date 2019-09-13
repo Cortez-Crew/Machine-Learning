b0cbfa4ebf180ffa8fa8302ddd217d96b93e35d3
Eddie Federmeyer <eddiefedermeyer@yahoo.com>
2019-09-13 11:06:49 -0500

    Added GitIgnore file

--- fashion.py
+++ fashion.py
@@ -17,8 +17,4 @@ print("TensorFlow Version: " + tf.__version__)
 fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-np.print_function(test_images[1])
-
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-
-
--- ml.py
+++ ml.py
@@ -1,4 +1,5 @@
 """
+This is my Basic Template for any of my Machine Learning Scripts
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 
