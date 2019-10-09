c0e047ba9e8a2aa68fb432266654af89accf7e8f
Eddie Federmeyer <eddiefedermeyer@yahoo.com>
2019-10-09 11:18:31 -0500

    TYPE

---
+++ #notes/1.tensor_types_nn.py
@@ -0,0 +1,15 @@
+"""
+My notes for TensorFlow
+https://github.com/easy-tensorflow/easy-tensorflow/
+↖ ↗ ↘ ↙
+
+"""
+
+import tensorflow.compat.v1 as tf
+
+"""
+    
+"""
+
+
+def neural_network_basics():
