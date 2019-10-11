9e20bd0124fc610d116de842ae645d24b73bdfee
Eddie Federmeyer <eddiefedermeyer@yahoo.com>
2019-10-11 11:49:15 -0500

    New Thing

---
+++ #notes/2.tensorboard.py
@@ -0,0 +1,18 @@
+"""
+My notes for TensorFlow
+https://github.com/easy-tensorflow/easy-tensorflow/
+↖ ↗ ↘ ↙
+
+"""
+
+import tensorflow.compat.v1 as tf
+
+"""
+
+
+"""
+
+
+def tensorboard_basics():
+    print('')
+
